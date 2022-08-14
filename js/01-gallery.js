85d35723b1f7359b0b172db2ff4c545807270b66
Juliya Khanenko <jullietnm@gmail.com>
2022-08-14 16:30:51 +0300

    ГОТОВО

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,9 +8,6 @@ const galleryList = createGalleryCardsItem(galleryItems);
 gallery.insertAdjacentHTML("beforeend", galleryList);
 
 
-
-
-
  function createGalleryCardsItem(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
@@ -30,81 +27,4 @@ gallery.insertAdjacentHTML("beforeend", galleryList);
  }
 
 
-// gallery.addEventListener("click",onGalleryLightBoxClick);
-
-//  const imgSource = event.target.dataset.source;
-
-//   const instance = basicLightbox.create(`
-//     <img
-//     src="${imgSource}"
-//     >
-// `);
-
-
-
-
-
-
-
-
-
-
-
-
-//  function onPictureClick(event) {
-//   event.preventDefault();
-//   if (!event.target.classList.contains('gallery__image')) {
-//     return;
-//   }
-//   // console.log(event.target);
-//   window.addEventListener('keydown', onEscBtnPress);
-//   modalRef.classList.add('is-open');
-//   imageRef.src = event.target.dataset.source;
-// }
-
-// function onCloseBtnClick() {
-//   modalRef.classList.remove('is-open');
-//   window.removeEventListener('keydown', onEscBtnPress);
-//   imageRef.src = '';
-// }
-
-// function onBackdropClick(event) {
-//   if (backdropRef === event.target) {
-//     onCloseBtnClick();
-//   }
-// }
-// function onEscBtnPress(event) {
-//   console.log(event);
-//   if (event.code === 'Escape') {
-//     onCloseBtnClick();
-//   }
-// }
-
-
-
-
-
-
-
-// const gallery = document.querySelector('.gallery')
-// const items = []
-
-// galleryItems.forEach(element => {
-// 	const galleryLink = document.createElement('a')
-// 	galleryLink.className = 'gallery__link'
-// 	galleryLink.href = element.original
-// 	const galleryImage = document.createElement('img')
-// 	galleryImage.className = 'gallery__image'
-// 	galleryImage.src = element.preview
-// 	galleryImage.setAttribute('title', element.description)
-// 	galleryImage.alt = element.description
-
-// 	galleryLink.append(galleryImage)
-// 	items.push(galleryLink)
-// })
-// gallery.append(...items)
-
-// new SimpleLightbox('.gallery a', {
-// 	captionDelay:250
-// })
 
